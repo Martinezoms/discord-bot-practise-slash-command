0c8a479a7754a938d13b48504c1f962c5fe959dd
Zoms <martinezoms15@gmail.com>
2022-05-08 12:36:32 +0100

    added database(MongoDb)

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import DiscordJs, {Intents} from "discord.js"
+import mongoose from "mongoose"
 import WOKCommands from "wokcommands"
 import path from 'path'
 import dotenv from "dotenv"
+
+import testSchema from './test-schema'
 
 // Initializing dotenv
 dotenv.config()
@@ -14,78 +17,26 @@ const client = new DiscordJs.Client({
   ]
 })
 
-client.on('ready', () => {
-  console.log('Bot is ready')
+client.on('ready', async() => {
+  // await mongoose.connect(process.env.MONGO_URI || '', {
+  //   keepAlive: true,
+
+  // })
 
   new WOKCommands(client, {
     commandDir: path.join(__dirname, 'commands'),
     typeScript: true,
-    testServers: '970064163428769843'
+    testServers: ['970064163428769843'],
+    botOwners: ['626830641949835264'],
+    mongoUri: process.env.MONGO_URI,
   })
 
-//   const guildId = '970064163428769843'
-//   const guild = client.guilds.cache.get(guildId)
-//   let commands
+  setTimeout(async() => {
+    await new testSchema({
+      message: 'Hello world'
+    }).save()
+  }, 1000)
 
-//   if(guild) {
-//     commands = guild.commands
-//   }else {
-//     commands = client.application?.commands
-//   }
-
-//   //Creating a slash command
-//   commands?.create({
-//     name: 'ping',
-//     description: 'Replies with pong'
-//   })
-
-//   // Adding two numbers command
-//   commands?.create({
-//     name: 'add',
-//     description: 'Adds two numbers',
-//     options: [
-//       {
-//         name: 'num1',
-//         description: 'The first number',
-//         required: true,
-//         type: DiscordJs.Constants.ApplicationCommandOptionTypes.NUMBER
-//       },
-//       {
-//         name: 'num2',
-//         description: 'The second number',
-//         required: true,
-//         type: DiscordJs.Constants.ApplicationCommandOptionTypes.NUMBER
-//       }
-//     ]
-//   })
-// })
-
-// client.on('interactionCreate', async(interaction) => {
-//   if(!interaction.isCommand()) {
-//     return;
-//   }
-
-//   const {commandName, options} = interaction
-
-//   if(commandName === 'ping') {
-//     interaction.reply({
-//       content: 'pong',
-//       ephemeral: true
-//     })
-//   }else if(commandName === 'add') {
-//     const num1 = options.getNumber('num1')!
-//     const num2 = options.getNumber('num2')!
-
-//     await interaction.deferReply({
-//       ephemeral: true
-//     })
-
-//     await new Promise(resolve => setTimeout(resolve, 5000))
-
-//     await interaction.editReply({
-//       content: `Great!...The sum is ${num1 + num2}`,
-//     })
-//   }
 
 
 })
